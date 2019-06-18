541c2979a154a717ffb34c63bf6397192ac078b3
TaniaTenorio <taniatenorio@hotmail.com>
2019-06-17 22:22:08 -0500

    correccion de errores arrojados despues de npm run test

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,4 @@
 window.cipher = {
-  
    encode: (password, codigo) => {
     //  let password="ABC";
     //  let codigo = 2;
@@ -9,7 +8,7 @@ window.cipher = {
            let char = (password.charAt(i));
           // console.log(char);
        let positionAscii = char.charCodeAt();
-       let newPositionAscii = ((positionAscii-32+offset)%95 + 32);
+       let newPositionAscii = ((positionAscii-65+offset)%26 + 65);
         let newCharacter = String.fromCharCode(newPositionAscii);
       //   newPassword = newCharacter.concat();
        newPassword = newPassword.concat(newCharacter);
@@ -18,18 +17,16 @@ window.cipher = {
        return newPassword;
       // console.log(newPassword);   
       },
-  
       // console.log(code("ABC",2));
       // document.write (code("ABC",2));
-
   decode: (password,codigo) => {
-    let offset = codigo;
+    let offset = parseInt(codigo);
    let newPassword= "";
    for(let i=0; i<password.length; i++){
         let char = (password.charAt(i));
        // console.log(char);
     let positionAscii = char.charCodeAt();
-    let newPositionAscii = ((positionAscii-32-offset)%95 + 32);
+    let newPositionAscii = ((positionAscii-65-offset)%26 + 65);
      let newCharacter = String.fromCharCode(newPositionAscii);
    //   newPassword = newCharacter.concat();
     newPassword = newPassword.concat(newCharacter);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-let password = document.getElementById("contrasena");
+let password = document.getElementById("contraseña");
 let codigo = document.getElementById("codigo");
+let newPassword = document.getElementById("contraseña-cifrada");
 let cifrar = document.getElementById("cifrar-button");
-// let descifrar = document.getElementById("descifrar-button");
-// let newPassword = document.getElementById("contraseña-cifrada");
+let codigo2 = document.getElementById("codigo2");
+let descifrar = document.getElementById("descifrar-button");
+let codedPassword = document.getElementById("contraseña-cifrada");
+let newCodedPassword = document.getElementById("contraseña-descifrada");
 // console.log(password);
-
 // const showNewPassword = () => {
-    
-    // };
-    
+    // };  
     cifrar.addEventListener("click", () => {
         password=password.value;
         codigo=codigo.value;
         // console.log(password)
         // console.log(codigo);
-         window.cipher.code(password,codigo).innerHMTL;
-        //  newPassword = printNewPassword;
-        //   console.log(newPassword);
+        // newPassword = newPassword.value
+        // newPassword.innerHTML = window.cipher.encode(password,codigo);
+        newPassword.innerHTML = window.cipher.encode(password,codigo); 
 });
-
-
-// console.log (showNewPassword);
-
-
-
+    descifrar.addEventListener("click", () => {
+        codedPassword = codedPassword.value;
+        codigo2=codigo2.value;
+        // console.log(codedPassword);
+        // console.log(codigo2);
+        newCodedPassword.innerHTML = window.cipher.decode(codedPassword,codigo2); 
+    // newPassword = newPassword.value
+    // newPassword.innerHTML = window.cipher.encode(password,codigo);   
+});
